6d73da6f67f2b42dae796d5f30d627ea33e7d9c0
Hunter Cox <huntersamuelcox1992@gmail.com>
2020-11-11 18:31:30 -0500

    Add social links to footer using font-awesome icons. Add site link to footer.

--- wp-content/themes/hsc2020/footer.php
+++ wp-content/themes/hsc2020/footer.php
@@ -13,17 +13,54 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hsc2020' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'hsc2020' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'hsc2020' ), 'hsc2020', '<a href="http://underscores.me/">Hunter Cox</a>' );
-				?>
+			<?php 
+				$site_url  = get_site_url();
+				$site_name = get_bloginfo();
+				echo '<a href="'. esc_url( $site_url ) .'">'.$site_name.'</a>';
+			?>
+		</div><!-- /.site-info -->
+			<?php 
+				// Add icon-links for: 
+				//   -- Github
+				//   -- LinkedIn
+				//   -- Instagram
+				//   -- Facebook
+
+				// Location: ACF Options page "my-custom-data"
+				$social_links = get_field('social_media_links', 'option');
+				if( $social_links ) :
+					echo '<ul class="footer__social-links">';
+
+					$github 	 = $social_links['github'];
+					$linkedin  = $social_links['linkedin'];
+					$instagram = $social_links['instagram'];
+					$facebook  = $social_links['facebook'];
+					
+					if( $github ) {
+						echo '<li><a href="'.esc_url($github).'" target="_blank">';
+							echo '<i class="fab fa-github-square"></i>';
+						echo '</a></li>';
+					}
+					if( $linkedin ) {
+						echo '<li><a href="'.$linkedin.'" target="_blank">';
+							echo '<i class="fab fa-linkedin"></i>';
+						echo '</a></li>';
+					}
+					if( $instagram ) {
+						echo '<li><a href="'.$instagram.'" target="_blank">';
+							echo '<i class="fab fa-instagram-square"></i>';
+						echo '</a></li>';
+					}
+					if( $facebook ) {
+						echo '<li><a href="'.$facebook.'" target="_blank">';
+							echo '<i class="fab fa-facebook-square"></i>';
+						echo '</a></li>';
+					}
+					
+					echo '</ul><!-- .footer__social-links -->';
+				endif;
+
+			?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
