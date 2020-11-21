95c6eca164bf86b6d3924210df22c2d5de8fe579
Hunter Cox <huntersamuelcox1992@gmail.com>
2020-11-21 11:35:52 -0500

    Style mobile dropdown nav menu and hamburger button.

--- wp-content/themes/hsc2020/js/my-scripts.js
+++ wp-content/themes/hsc2020/js/my-scripts.js
@@ -1,7 +1,16 @@
 (function($) {
-	// Projects 
+  
+  // Header - Hamburger menu
+    $('.hamburger--squeeze').on('click', function() {
+      $(this).toggleClass('is-active');
+      $(this).next('.main-navigation').slideToggle();
+    })
+  
+  
+  // Projects 
     $('.toggler').on('click', function() {
       $(this).next('.toggle-target').slideToggle();
-    })
+    });
+
       
   })( jQuery );
